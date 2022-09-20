f78d22dda0bcd5c9930e6df8aa69e161e9a89e6a
Ahmed Elhady <ahmedmohamed123217@gmail.com>
2022-09-20 10:51:15 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Card from "./components/Card.js";
+import data from "./data.json";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            {data["courses"].map((course) => {
+                return <Card course={course}></Card>;
+            })}
+        </div>
+    );
 }
 
 export default App;
